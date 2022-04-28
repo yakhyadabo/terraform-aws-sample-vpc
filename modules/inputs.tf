4f943bcc859e1913f0c87d14577f8523842bfff2
Yakhya Dabo <yakhyadabo@gmail.com>
2022-04-28 21:15:48 +0100

    [module] new variables

--- modules/inputs.tf
+++ modules/inputs.tf
@@ -5,3 +5,21 @@ variable "environment" {
 variable "vpc_cidr" {
   description = "CIDR block of the vpc"
 }
+
+variable "public_subnets_cidr" {
+  type        = list(any)
+  description = "CIDR block for Public Subnet"
+}
+
+variable "private_subnets_cidr" {
+  type        = list(any)
+  description = "CIDR block for Private Subnet"
+}
+
+variable "availability_zones" {
+  type        = list(any)
+  description = "AZ in which all the resources will be deployed"
+}
+variable "region" {
+  default = "us-east-1"
+}
