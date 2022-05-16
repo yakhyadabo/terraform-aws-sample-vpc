66d392d1ccf91c98d40aa166a3eacb57d935fcc5
Yakhya Dabo <yakhyadabo@gmail.com>
2022-05-16 21:55:01 +0100

    [instance] Dev

---
+++ dev.tf
@@ -0,0 +1,17 @@
+resource "random_id" "random_id_prefix" {
+  byte_length = 2
+}
+
+locals {
+  production_availability_zones = ["${var.region}a", "${var.region}b", "${var.region}c"]
+}
+
+module "vpc" {
+  source               = "./module"
+  region               = var.region
+  environment          = var.project.environment
+  vpc_cidr             = var.vpc_cidr
+  public_subnets_cidr  = var.public_subnets_cidr
+  private_subnets_cidr = var.private_subnets_cidr
+  availability_zones   = local.production_availability_zones
+}
