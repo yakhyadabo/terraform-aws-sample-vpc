77e1a4c3495f847cd7a66cdd53a01207f4aa431d
Yakhya Dabo <yakhyadabo@gmail.com>
2023-01-31 01:36:04 +0000

    [instance] update params

--- dev.tf
+++ dev.tf
@@ -9,9 +9,11 @@ locals {
 module "vpc" {
   source               = "./module"
   region               = var.region
+  project_name         = var.project.name
   environment          = var.project.environment
   vpc_cidr             = var.vpc_cidr
   public_subnets_cidr  = var.public_subnets_cidr
   private_subnets_cidr = var.private_subnets_cidr
   availability_zones   = local.production_availability_zones
+  eks_cluster_subnets_cidr = var.eks_cluster_subnets_cidr
 }
